cd66ab601fd82c0d1eb75949fac90ce90ab730e9
xiaao <xiaao@huayuncc.com>
2021-09-17 14:05:58 +0800

    xxx

--- XVideoPlayerDemo/VideoPlayerViewController.swift
+++ XVideoPlayerDemo/VideoPlayerViewController.swift
@@ -27,7 +27,7 @@ class VideoPlayerViewController: UIViewController {
         videoView.addSubview(videoPlayerViewController.view)
         videoPlayerViewController.setupPlayer()
         videoPlayerViewController.setTitle("Video Play")
-        videoPlayerViewController.setVideoUrl("http://zhy-media.meldingcloud.com/video/720p/1706f391e736415abc25cce022660f93/transcode_1534406970207/b751afc3-11ca-4d8d-8d69-c314222fe4ee.mp4")
+        videoPlayerViewController.setVideoUrl("https://mim-img3.cctv.cn/video/720p/3d78fe6326474775bc789c87dfbbf0c4/TRANSCODE_1615269129757/26c28f75-df67-487b-ae03-373ba025c20e.mp4")
 //        videoPlayerViewController.setVideoUrl("http://playertest.longtailvideo.com/adaptive/bipbop/gear4/prog_index.m3u8")
         videoPlayerViewController.play()
     }
